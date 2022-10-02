371120e521cf9ef93513317730f0242d1053d813
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-02 23:20:47 +0900

    BoardAction class and its member variables added

--- Board.java
+++ Board.java
@@ -1,15 +1,51 @@
 import java.util.Scanner;
 
+class BoardAction {
+    private String title;
+    private String content;
+    private String author;
+
+    BoardAction() {
+
+    }
+    BoardAction(String title, String content, String author) {
+       this.title = title;
+       this.content = content;
+       this.author = author;
+    }
+
+    public BoardAction prompt(Scanner reader) {
+        String title;
+        String content;
+        String author;
+
+        System.out.print("제목> ");
+        title = reader.next();
+        System.out.print("내용> ");
+        content = reader.next();
+        System.out.print("작성자> ");
+        author = reader.next();
+
+        return new BoardAction(title, content, author);
+    }
+}
+
 public class Board {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        BoardAction board = new BoardAction();
 
         System.out.println("======================");
         System.out.println("번호  제목  작성자  작성일");
         System.out.println("----------------------");
         System.out.print("1.목록  2.등록  3.내용  4.삭제  0.종료 >");
 
-        sc.nextInt();
+        int input = sc.nextInt();
+
+        if (input == 1) {
+            board = board.prompt(sc);
+        }
     }
 
 }
